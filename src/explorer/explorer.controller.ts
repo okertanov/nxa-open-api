30a3542ee349c3d73dfdcbe28d51e9e535e95e17
Oleg Kertanov <okertanov@gmail.com>
2021-09-02 16:53:03 +0300

    DVITA-51 - API: Create Initial Blockchain Access API Layer (WIP for Explorer)

--- src/explorer/explorer.controller.ts
+++ src/explorer/explorer.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Logger, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, Req } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
+import { BlockchainAssetDto } from 'src/assets/dto/blockchain.asset.dto';
+import { BlockchainSmartContract } from 'src/blockchain/types/blockchain.smart.contract';
+import { BlockchainBlock } from '../blockchain/types/blockchain.block';
+import { BlockchainTransaction } from '../blockchain/types/blockchain.transaction';
 import { ExplorerService } from './explorer.service';
 
 @ApiTags('Explorer')
@@ -9,5 +13,97 @@ export class ExplorerController {
     private readonly logger = new Logger(ExplorerController.name);
 
     constructor(private readonly explorerService: ExplorerService) {
+    }
+
+    @Get('/blocks')
+    @ApiOperation({ summary: 'Get paginated blocks for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated blocks for explorer', type: BlockchainBlock, isArray: true })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getBlocks(
+        @Req() req: Request
+    ): Promise<BlockchainBlock[]> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getBlocks();
+    }
+
+    @Get('/block/:hash')
+    @ApiOperation({ summary: 'Get paginated blocks for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated blocks for explorer', type: BlockchainBlock })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getBlock(
+        @Req() req: Request,
+        @Param('hash') hash: string
+    ): Promise<BlockchainBlock> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getBlock(hash);
+    }
+
+    @Get('/transactions')
+    @ApiOperation({ summary: 'Get paginated transactions for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated transactions for explorer', type: BlockchainTransaction, isArray: true })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getTransactions(
+        @Req() req: Request
+    ): Promise<BlockchainTransaction[]> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getTransactions();
+    }
+
+    @Get('/transaction/:hash')
+    @ApiOperation({ summary: 'Get paginated transaction for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated transaction for explorer', type: BlockchainTransaction })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getTransaction(
+        @Req() req: Request,
+        @Param('hash') hash: string
+    ): Promise<BlockchainTransaction> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getTransaction(hash);
+    }
+
+    @Get('/contracts')
+    @ApiOperation({ summary: 'Get paginated contracts for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated transactions for explorer', type: BlockchainSmartContract, isArray: true })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getContracts(
+        @Req() req: Request
+    ): Promise<BlockchainSmartContract[]> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getContracts();
+    }
+
+    @Get('/contract/:hash')
+    @ApiOperation({ summary: 'Get paginated contract for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated contract for explorer', type: BlockchainSmartContract })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getContract(
+        @Req() req: Request,
+        @Param('hash') hash: string
+    ): Promise<BlockchainSmartContract> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getContract(hash);
+    }
+
+    @Get('/tokens')
+    @ApiOperation({ summary: 'Get paginated tokens for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated tokens for explorer', type: BlockchainAssetDto, isArray: true })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getTokens(
+        @Req() req: Request
+    ): Promise<BlockchainAssetDto[]> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getTokens();
+    }
+
+    @Get('/token/:hash')
+    @ApiOperation({ summary: 'Get paginated token for explorer' })
+    @ApiResponse({ status: 200, description: 'The paginated token for explorer', type: BlockchainAssetDto })
+    @ApiResponse({ status: 500, description: 'Internal Server Error' })
+    async getToken(
+        @Req() req: Request,
+        @Param('hash') hash: string
+    ): Promise<BlockchainAssetDto> {
+        this.logger.verbose(`${req.method} : ${req.url}`);
+        return this.explorerService.getToken(hash);
     }
 }
--- src/explorer/explorer.service.ts
+++ src/explorer/explorer.service.ts
@@ -1,5 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { BlockchainSmartContract } from '../blockchain/types/blockchain.smart.contract';
+import { BlockchainBlock } from '../blockchain/types/blockchain.block';
+import { BlockchainTransaction } from '../blockchain/types/blockchain.transaction';
+import { BlockchainAssetDto } from '../assets/dto/blockchain.asset.dto';
 
 @Injectable()
 export class ExplorerService {
+    async getBlocks(): Promise<BlockchainBlock[]> {
+        return [];
+    }
+
+    async getBlock(hash: string): Promise<BlockchainBlock> {
+        return undefined;
+    }
+
+    async getTransactions(): Promise<BlockchainTransaction[]> {
+        return [];
+    }
+
+    async getTransaction(hash: string): Promise<BlockchainTransaction> {
+        return undefined;
+    }
+
+    async getContracts(): Promise<BlockchainSmartContract[]> {
+        return [];
+    }
+
+    async getContract(hash: string): Promise<BlockchainSmartContract> {
+        return undefined;
+    }
+
+    async getTokens(): Promise<BlockchainAssetDto[]> {
+        return [];
+    }
+
+    async getToken(hashOrCode: string): Promise<BlockchainAssetDto> {
+        return undefined;
+    }
 }
