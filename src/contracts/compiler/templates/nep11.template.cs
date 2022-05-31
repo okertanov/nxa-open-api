3615c823a57565f8c95c90e614879d4d599ae28b
Oleg Kertanov <okertanov@gmail.com>
2022-05-31 15:35:58 +0300

    Fix for NEP11 token compilation 3

--- src/contracts/compiler/templates/nep11.template.cs
+++ src/contracts/compiler/templates/nep11.template.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Numerics;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Neo;
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Native;
